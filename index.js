dfe7e93de11a09482e32534ab1a094b281368dd0
Matteo Maronati <me@mattinthe.net>
2017-06-10 15:55:20 +0200

    Up: basic save svg implementation

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('app-module-path').addPath(__dirname)
 // libs
 var Express = require('express')
 var _ = require('lodash')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var fs = require('fs')
 // env
 var env
 try { env = require('.env') }
@@ -16,14 +19,36 @@ class App {
   constructor () {
     this.app = new Express()
     this.router = Express.Router()
-    this.app.use('/', this.router)
+    this.app.use(cors())
+    this.app.use(bodyParser.json())
+    this.app.use('/api', this.router)
     this.routes = {}
     this.init()
   }
 
   init () {
+    this.router.route
+    this.app.use(function(req, res, next) {
+      next();
+    });
+    this.router.route('/post/svg').post((req, res) => {
+      if (!fs.existsSync(`./data/${req.body.id}`)){
+        fs.mkdirSync(`./data/${req.body.id}`);
+      }
+      fs.writeFile(`./data/${req.body.id}/${req.body.time}.svg`, req.body.raw, function(err) {
+      console.log(err);
+      });
+      res.send('Svg saved')
+    })
+
+    this.app.use(Express.static('./frontend/dist'))
+    this.app.use('/play/:id', Express.static('./frontend/dist'))
     this.app.use('/', Express.static('./frontend/dist'))
 
+    let getAll = (req, res) => {
+      res.redirect('/')
+    }
+    this.app.use(getAll)
     this.app.listen(env.port, () => {
     })
 
