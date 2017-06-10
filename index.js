5ddd80cb921a2fe0d77e81170d49f71e97642313
Matteo Maronati <me@mattinthe.net>
2017-06-10 16:34:49 +0200

    Up: implemented zip download

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var _ = require('lodash')
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var fs = require('fs')
+var path = require('path')
+var zip = require('zip-folder')
 // env
 var env
 try { env = require('.env') }
@@ -27,31 +29,44 @@ class App {
   }
 
   init () {
-    this.router.route
-    this.app.use(function(req, res, next) {
-      next();
-    });
+    this.app.use(Express.static('./frontend/dist'))
+    this.app.use('/play/:id', Express.static('./frontend/dist'))
+    this.app.use('/zips', Express.static('./zips'))
+    this.app.use('/', Express.static('./frontend/dist'))
+
+    let swallowAll = (req, res) => {
+      res.redirect('/')
+    }
+
+    this._addApiRoutes()
+    this.app.use(swallowAll)
+    this.app.listen(env.port)
+  }
+
+  _addApiRoutes () {
+
+    // save svg
     this.router.route('/post/svg').post((req, res) => {
       if (!fs.existsSync(`./data/${req.body.id}`)){
         fs.mkdirSync(`./data/${req.body.id}`);
       }
       fs.writeFile(`./data/${req.body.id}/${req.body.time}.svg`, req.body.raw, function(err) {
-      console.log(err);
+        console.log(err);
       });
       res.send('Svg saved')
     })
 
-    this.app.use(Express.static('./frontend/dist'))
-    this.app.use('/play/:id', Express.static('./frontend/dist'))
-    this.app.use('/', Express.static('./frontend/dist'))
-
-    let getAll = (req, res) => {
-      res.redirect('/')
-    }
-    this.app.use(getAll)
-    this.app.listen(env.port, () => {
+    // get zip
+    this.router.route('/get/zip/:id').get((req, res) => {
+      res.header('Content-Type', 'application/zip')
+      if (fs.existsSync(`./data/${req.params.id}`)) {
+        zip(`./data/${req.params.id}`, `./zips/${req.params.id}.zip`, (err) => {
+          if (!err) {
+            res.send(req.protocol + '://' + req.get('host') + `/zips/${req.params.id}.zip`)
+          }
+        })
+      }
     })
-
   }
 
 
