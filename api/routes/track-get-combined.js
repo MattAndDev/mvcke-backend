fc2d1bccda7581ea391f224946cc2f97d762ddd6
Matteo Maronati <me@mattinthe.net>
2017-06-12 22:53:59 +0200

    Rw: restructuring and cleaning track-get-combined

--- api/routes/track-get-combined.js
+++ api/routes/track-get-combined.js
@@ -12,35 +12,48 @@ PDFDocument.prototype.addSVG = function(svg, x, y, options) {
 };
 
 module.exports = function (router) {
-    // // get zip
-    router.route('/track/get/combined/:id').get((req, res) => {
-      res.header('Content-Type', 'application/zip')
-      if (fs.existsSync(`./data/${req.params.id}`)) {
-        var doc = new PDFDocument({size: [2030,2900]}) // somewhat 70/100 cm
-        doc.pipe(fs.createWriteStream(`./data/${req.params.id}/poster.pdf`))
-        this.files = []
-        glob(`./data/${req.params.id}/*.svg`, {nosort: true}, (err, files) => {
-          _.each(files, (file, index) => {
-            let identifier = path.basename(file).replace('.svg','')
-            console.log(identifier);
-              fs.readFile(file, 'utf8',  (err,data) => {
-                if (err)  return console.log(err)
-                this.files.push({
-                  name: identifier,
-                  data: data
-                })
-                if (index === files.length - 1 ) {
-                  _.each(this.files, (file, index) => {
-                    console.log(sizeOf(files[index]));
-                    doc.addSVG(file.data, 0,0, {assumePt: true});
-                  })
-                  doc.end()
-                  console.log('done');
-                }
-              });
+
+
+  // gets all svg in folder, callbacks an array of objects
+  // {
+  //  time: time in second of the saved svg (leading zeroes),
+  //  raw: raw svg
+  // }
+  let svgsToArray = (cb) => {
+    let jsonFiles = []
+    glob(`./data/${this.id}/*.svg`, {nosort: true}, (err, files) => {
+      _.each(files, (file, index) => {
+        let identifier = path.basename(file).replace('.svg','')
+        fs.readFile(file, 'utf8',  (err,data) => {
+          if (err)  return console.log(err)
+          jsonFiles.push({
+            time: identifier,
+            raw: data
           })
-          res.send('Svg saved')
-        })
-      }
+          if (index === files.length - 1 ) {
+            cb(jsonFiles)
+          }
+        });
+      })
     })
+  }
+
+  // 
+  router.route('/track/get/combined/:id').get((req, res) => {
+    res.header('Content-Type', 'application/zip')
+    if (fs.existsSync(`./data/${req.params.id}`)) {
+      this.id = req.params.id
+      // get files into array
+      svgsToArray((files) => {
+        // setup moc pdf
+        let doc = new PDFDocument({size: [2030,2900]}) // somewhat 70/100 cm
+        doc.pipe(fs.createWriteStream(`./data/${this.id}/poster.pdf`))
+        _.each(files, (file, index) => {
+          doc.addSVG(file.raw, 0,0, {assumePt: true});
+        })
+        doc.end()
+        res.send('Svg saved')
+      })
+    }
+  })
 }
